7b0e81ae4f18148095aa50dfd10064b61d260474
penkovski <l_penkovski@yahoo.com>
2019-09-19 23:24:37 +0300

    Add msg package documentation.

---
+++ pkg/msg/doc.go
@@ -0,0 +1,7 @@
+// Package msg implements the messages and auxiliary structures of the Bitcoin protocol.
+//
+// The Bitcoin protocol specification describes the messages and their formats
+// which peers in the network exchange among themselves. This package aims to implement
+// the data structures and serialization/deserialization functions needed to create and
+// receive messages in the network.
+package msg
